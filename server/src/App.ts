e64f710790e99a63734d9ed2c3832101c51f4d0f
Arthur <SolomonAndrew@tutanota.com>
2023-03-22 11:34:56 +0200

    init server app add typescript configuration, define project folder structure

---
+++ server/src/App.ts
@@ -0,0 +1,12 @@
+class App {
+    constructor() {
+        this.boot();
+    }
+
+
+    private boot(): void {
+        console.log('Your express application successfully setup on port 3000')
+    }
+};
+
+new App();
